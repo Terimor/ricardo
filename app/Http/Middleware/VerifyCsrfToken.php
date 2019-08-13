36c7316ec1fa3e3e470dc14f0f0b65d2ae53b242
Stanislau Karaliou <lorderetik@gmail.com>
2019-08-13 14:37:30 +0300

    generate bluesnap token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -18,7 +18,6 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [
-        '/payments/bluesnap-generate-token'
+    protected $except = [        
     ];
 }
