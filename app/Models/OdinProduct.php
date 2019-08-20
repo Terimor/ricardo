ed4a53fef2c847913dee7a1c04425c3b96143462
Vitaly <vitaly@on-5.com>
2019-08-20 17:55:05 +0300

    Product prices fix

--- app/Models/OdinProduct.php
+++ app/Models/OdinProduct.php
@@ -11,15 +11,15 @@ use App\Services\CurrencyService;
  */
 class OdinProduct extends Model
 {
-    
+
     const QUANTITY_PRICES = 5;
     public $images;
-    
+
     protected $fillable = [
         'product_name', 'description', 'long_name', 'is_digital', 'is_hidden_checkout', 'logo_image_id', 'billing_descriptor', 'qty_default', 'is_shipping_cost_only', 'is_3ds_required', 'is_hygiene', 'is_bluesnap_hidden', 'is_paypal_hidden', 'category_id', 'vimeo_id', 'warehouse_id', 'warranty_percent', 'skus', 'prices', 'fb_pixel_id', 'gads_retarget_id', 'gads_conversion_id', 'gads_conversion_label', 'upsell_plusone_text', 'upsell_hero_text', 'upsell_hero_image_id', 'upsells', 'currency'
     ];
-    
-    
+
+
     /**
      * @var string
      */
@@ -101,7 +101,7 @@ class OdinProduct extends Model
 
         return $value;
     }
-    
+
     /**
      * Getter prices
      * @param type $value
@@ -126,20 +126,20 @@ class OdinProduct extends Model
             }
             $value[$key]['currency'] = $currency->code;
             $value[$key]['exchange_rate'] = $currency->usd_rate;
-            
+
             if (!request()->has('cop_id') || $val['price_set'] == request()->get('cop_id')) {
                 $returnedKey = $key;
-                break;                
+                break;
             }
         }
 
         if (request()->has('cop_id')) {
             logger()->error("Cop id ".request()->get('cop_id')." not found");
-        }        
-   
+        }
+
         return $value[$returnedKey];
     }
-    
+
     /**
      * Set local images for object
      */
@@ -153,29 +153,31 @@ class OdinProduct extends Model
         if(!empty($this->upsell_hero_image_id)) {
             $ids[$this->upsell_hero_image_id] = $this->upsell_hero_image_id;
         }
-        
+
         // for prices
         $returnedKey = 0;
-        foreach ($this->attributes['prices'] as $key => $val) {
-            if (!request()->has('cop_id') || $val['price_set'] == request()->get('cop_id')) {
-                for ($i=1; $i <= self::QUANTITY_PRICES; $i++) {
-                    if (!empty($val[$i]['image_id'])) {                        
-                        $ids[$val[$i]['image_id']] = $val[$i]['image_id'];                    
-                    }
-                }
-                $returnedKey = $key;
-                break;                
-            }
+        if (!empty($this->attributes['prices'])) {
+          foreach ($this->attributes['prices'] as $key => $val) {
+              if (!request()->has('cop_id') || $val['price_set'] == request()->get('cop_id')) {
+                  for ($i=1; $i <= self::QUANTITY_PRICES; $i++) {
+                      if (!empty($val[$i]['image_id'])) {
+                          $ids[$val[$i]['image_id']] = $val[$i]['image_id'];
+                      }
+                  }
+                  $returnedKey = $key;
+                  break;
+              }
+          }
         }
         if ($ids) {
             $this->images = [];
             $this->imagesObjects = AwsImage::whereIn('_id', $ids)->get();
             foreach ($this->imagesObjects as $image) {
-                $this->images[$image->id] = !empty($image['urls'][app()->getLocale()]) ? $image['urls'][app()->getLocale()] : !empty($image['urls']['en']) ? $image['urls']['en'] : '';                
+                $this->images[$image->id] = !empty($image['urls'][app()->getLocale()]) ? $image['urls'][app()->getLocale()] : !empty($image['urls']['en']) ? $image['urls']['en'] : '';
             }
         }
     }
-    
+
     /**
      * Getter logo image
      */
@@ -183,12 +185,12 @@ class OdinProduct extends Model
     {
         return !empty($this->logo_image_id) ? $this->images[$this->logo_image_id] : null;
     }
-    
+
     /**
      * Getter upsell_hero_image
      */
     public function getUpsellHeroImageAttribute($value)
     {
         return !empty($this->upsell_hero_image_id) ? $this->images[$this->upsell_hero_image_id] : null;
-    }    
+    }
 }
