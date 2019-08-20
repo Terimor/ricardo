0330405e1ebf159b6eb7f5ae2e45e6ac8a2f5994
Volodymyr Radchenko <rreckonerr@gmail.com>
2019-08-20 19:20:16 +0300

    handle error on initialize EBANX

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -10,9 +10,14 @@
       'Authorization': 'Basic {{base64_encode(env('BLUESNAP_API_KEY').':'.env('BLUESNAP_API_PASS'))}}',
     }
 
-    EBANX.config.setMode('{{env('EBANX_MODE')}}');
-    EBANX.config.setPublishableKey('{{env('EBANX_SANDBOX_PUBLIC_INTEGRATION_KEY')}}');
-    EBANX.config.setCountry('br');
+    try {
+      EBANX.config.setMode('{{env('EBANX_MODE')}}');
+      EBANX.config.setPublishableKey('{{env('EBANX_SANDBOX_PUBLIC_INTEGRATION_KEY')}}');
+      EBANX.config.setCountry('br');
+    } catch (e) {
+      console.error('Error with initialize EBANX')
+      console.error(e)
+    }
 
     const checkoutData = {
       countryCode: '{{ $location->countryCode }}',
