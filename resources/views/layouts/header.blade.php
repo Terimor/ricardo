ef021038003ab45202717d908ce4de48778c862d
vladyslav.kostiuk <vladyslav.kostiuk@chisw.com>
2019-09-05 14:51:44 +0300

    back timer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container">
         <img src="{{$product->logo_image}}" alt="">
-        <!-- <timer-component></timer-component> -->
+        <timer-component></timer-component>
     </div>
 </header>
