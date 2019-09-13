ac966e728020adc5d1393d00d28a318fbee81af3
Bogdan G <bogdan.harasan@chisw.com>
2019-09-13 11:30:00 +0300

    sku image

--- resources/js/components/smc7.vue
+++ resources/js/components/smc7.vue
@@ -367,7 +367,7 @@
 		mounted() {
 			this.variantList = this.productData.skus.map((it) => ({
 				label: it.name,
-				text: `<div><img src="/images/headphones-white.png" alt=""><span>${it.name}</span></div>`,
+				text: `<div><img src="${it.quantity_image[1]}" alt=""><span>${it.name}</span></div>`,
 				value: it.code,
 				imageUrl: it.quantity_image[1]
 			}));
