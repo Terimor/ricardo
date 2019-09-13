0e057945bfc67a8a675b49e8447544feb2b2b6d9
Bogdan G <bogdan.harasan@chisw.com>
2019-09-13 12:11:12 +0300

    Removed payer id from an upper level of an order

--- app/Models/OdinOrder.php
+++ app/Models/OdinOrder.php
@@ -30,7 +30,6 @@ class OdinOrder extends OdinModel
         'shop_currency' => null, // enum string, //currency was used to display prices
         //'payment_provider' => null, // enum string
         //'payment_method' => null, // enum string
-        'payer_id' => null, // string, payer ID in payment provider system
         'installments' => 0,
         'customer_email' => null, // * string
         'customer_first_name' => null, // * string
@@ -120,7 +119,7 @@ class OdinOrder extends OdinModel
         'number', 'status', 'currency', 'exchange_rate', 'total_paid', 'total_price', 'total_price_usd', 'shop_currency',
         'customer_id', 'customer_email', 'customer_first_name', 'customer_last_name', 'customer_phone', 'language', 'ip', 'shipping_country',
         'shipping_zip', 'shipping_state', 'shipping_city', 'shipping_street', 'shipping_street2', 'exported', 'warehouse_id', 'trackings', 'products',
-        'ipqualityscore', 'page_checkout', 'flagged', 'offer', 'affiliate', 'is_refunding', 'is_refunded', 'qc_passed', 'txns', 'payer_id'
+        'ipqualityscore', 'page_checkout', 'flagged', 'offer', 'affiliate', 'is_refunding', 'is_refunded', 'qc_passed', 'txns'
 
     ];
 
