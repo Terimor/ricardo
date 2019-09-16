38681b059e744cb4650b80102fb70e52e1231be7
Vitaly <vitaly@on-5.com>
2019-09-16 14:40:04 +0300

    Disabled paypal verification

--- app/Http/Controllers/Payments/PaypalController.php
+++ app/Http/Controllers/Payments/PaypalController.php
@@ -58,7 +58,7 @@ class PaypalController extends Controller
     public function verifyOrder(PayPalVerfifyOrderRequest $request)
     {
         //return $this->payPalService->verifyOrder($request);
-return ['order_id' => 'fake-test'];
+return ['order_id' => '5d7f73a649436003f365dc84'];
     }
 
     /**
