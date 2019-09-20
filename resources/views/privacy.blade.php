349148be1f9f2eee53bede8ecee45f161eec535b
vladyslav.kostiuk <vladyslav.kostiuk@chisw.com>
2019-09-20 06:31:47 +0000

    Squashed commit of the following
    commit 726b9687 
    Author: vladyslav.kostiuk <vladyslav.kostiuk@chisw.com> 
    Date: Fri Sep 20 2019 08:56:17 GMT+0300 (Москва, стандартное время) 
    
        small fixes

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Returns')
+@section('title', $product->skus[0]['name'] . ' - ' . 'Privacy Policy')
 
 @section('styles')
     <link rel="stylesheet" href="{{ asset('css/static.css') }}">
@@ -187,7 +187,7 @@
             <br>
             <br>
 
-            @include('layouts.footer', ['isWhite' => true])
+            @include('layouts.footer', ['isWhite' => true, 'hasHome' => true ])
         </div>
     </div>
 </div>
