b1f5e7a73802d210f5f639d512d177232ab2924a
Bogdan G <bogdan.harasan@chisw.com>
2019-09-23 13:04:48 +0300

    odin customers recently bougth data

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,12 +2,9 @@
 
 namespace App\Providers;
 
-use App\Models\OdinCustomer;
-use App\Services\PayPalService;
 use App\Services\UtilsService;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 
 class ViewServiceProvider extends ServiceProvider
 {
