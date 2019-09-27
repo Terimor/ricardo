d8381b131a36bcab22b4a613271c9196714e830f
Viacheslav Alenchikov <valenchikov@gmail.com>
2019-09-27 09:02:09 +0000

    Squashed commit of the following
    commit 1e3fe5ae 
    Author: Viacheslav Alenchikov <valenchikov@gmail.com> 
    Date: Fri Sep 27 2019 11:58:46 GMT+0300 (Москва, стандартное время) 
    
        google analytics frontend integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,12 @@
 
     @yield('head')
 
+    @if (!empty($ga_id))
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id={{ $ga_id }}"></script>
+      <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','{{ $ga_id }}');</script>
+    @endif
+
     <!-- Scripts -->
 
     @if ($HasVueApp)
