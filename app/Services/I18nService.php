de9b3ca92bd60639769cb1ebedf4f1872f220542
Bogdan G <bogdan.harasan@chisw.com>
2019-10-01 12:06:45 +0000

    Squashed commit of the following
    commit 6ed6d5d4 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Tue Oct 01 2019 13:38:26 GMT+0300 (Москва, стандартное время) 
    
        Merge branch 'master' of https://git-codecommit.eu-central-1.amazonaws.com/v1/repos/ODIN into feature/244-new-languages
    
    
    commit 489142d5 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Tue Oct 01 2019 13:34:28 GMT+0300 (Москва, стандартное время) 
    
        Refactored special locale conditions
    
    
    commit 8dca41bb 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Mon Sep 30 2019 18:33:04 GMT+0300 (Москва, стандартное время) 
    
        locale
    
    
    commit c900df87 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Mon Sep 30 2019 18:28:18 GMT+0300 (Москва, стандартное время) 
    
        locale
    
    
    commit eebc52c7 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Mon Sep 30 2019 18:18:29 GMT+0300 (Москва, стандартное время) 
    
        added browser codes
    
    
    commit fd8ec9fa 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Mon Sep 30 2019 17:30:25 GMT+0300 (Москва, стандартное время) 
    
        removed HK
    
    
    commit 69338979 
    Author: Bogdan G <bogdan.harasan@chisw.com> 
    Date: Mon Sep 30 2019 17:07:17 GMT+0300 (Москва, стандартное время) 
    
        added new translations

--- app/Services/I18nService.php
+++ app/Services/I18nService.php
@@ -17,8 +17,7 @@ class I18nService
      */
     public static function loadPhrases(string $category)
     {
-        $locale = app()->getLocale();
-        $language = self::getLanguageByLocale($locale);
+        $language = app()->getLocale();
         if (empty(I18n::$loadedPhrases[$language])) {
             $categories = [$category];
             if (strpos($category, '_page')) {
@@ -64,17 +63,21 @@ class I18nService
     }
 
     /**
-    * Get translation phrase
-    * From saga I18n::t
-    * @param type $phrase
-    * @param type $lang
-    */
-   public static function getTranslatedPhrase(string $phrase, string $language = 'en', $args = []): string
-   {
+     * Get translation phrase
+     * From saga I18n::t
+     *
+     * @param string $phrase
+     * @param string $language
+     * @param array $args
+     *
+     * @return string
+     */
+    public static function getTranslatedPhrase(string $phrase, string $language = 'en', array $args = []): string
+    {
         $translation = $phrase;
-            $language = $language ? strtolower($language) : 'en';
+        $language = $language ? strtolower($language) : 'en';
 
-       if (isset(I18n::$browser_codes[$language])) {
+        if (isset(I18n::$browser_codes[$language])) {
             $language = I18n::$browser_codes[$language];
         }
 
@@ -84,19 +87,19 @@ class I18nService
         if (!empty($loadedPhrases[$translation])) {
             $translation = $loadedPhrases[$translation];
         } else {
-            logger()->error("URGENT: `{$translation}` not found in translations. Args: ".json_encode($args));
+            logger()->error("URGENT: `{$translation}` not found in translations. Args: " . json_encode($args));
         }
 
         if ($args) {
             foreach ($args as $key => $value) {
-            $placeholderKey = "#".strtoupper($key)."#";
-            $translation = str_replace($placeholderKey, $value, $translation);
-            if (!in_array($placeholderKey, I18n::$placeholders)) {
-                logger()->error("Non-registered placeholder {$placeholderKey}. Add it to I18n::placeholders!");
-            }
-            if ($value === null) {
-                logger()->error("Translation is null for {$placeholderKey}");
-            }
+                $placeholderKey = "#" . strtoupper($key) . "#";
+                $translation = str_replace($placeholderKey, $value, $translation);
+                if (!in_array($placeholderKey, I18n::$placeholders)) {
+                    logger()->error("Non-registered placeholder {$placeholderKey}. Add it to I18n::placeholders!");
+                }
+                if ($value === null) {
+                    logger()->error("Translation is null for {$placeholderKey}");
+                }
             }
         }
 
@@ -104,25 +107,21 @@ class I18nService
         if (substr_count($translation, '#') > 1) {
             $other_hashes_cnt = substr_count($translation, '/#/');
             if (substr_count($translation, '#') != $other_hashes_cnt) {
-            logger()->error("Non-translated placeholders for `{$phrase}`: {$translation}. Arguments: ". json_encode($args));
+                logger()->error("Non-translated placeholders for `{$phrase}`: {$translation}. Arguments: " . json_encode($args));
             }
         }
 
         return $translation;
-   }
+    }
 
     /**
      * Returns language by locale
      *
-     * @param string $locale
+     * @param string $language_code
      * @return string
      */
-   public static function getLanguageByLocale(string $locale): string
-   {
-       $mapping = [
-           'zh-tw' => 'tw'
-       ];
-
-       return !empty($mapping[$locale]) ? $mapping[$locale] : $locale;
-   }
+    public static function getInternalLanguageByLocaleLanguage(string $language_code): string
+    {
+        return !empty(I18n::$browser_codes[$language_code]) ? I18n::$browser_codes[$language_code] : $language_code;
+    }
 }
