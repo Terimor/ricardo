3d48b8b4566fccd76e32df06942a3694723927f7
Viacheslav Alenchikov <valenchikov@gmail.com>
2019-10-01 12:29:07 +0000

    Squashed commit of the following
    commit d5739159 
    Author: Viacheslav Alenchikov <valenchikov@gmail.com> 
    Date: Tue Oct 01 2019 15:20:04 GMT+0300 (Москва, стандартное время) 
    
        populate links with get params

--- public/js/static.js
+++ public/js/static.js
@@ -1,3 +1,7 @@
+(function() {
+
+
+// wait until condition returned true, then execute callback
 function wait(condition, callback, timeout = 100) {
   let interval = null;
 
@@ -21,7 +25,45 @@ function wait(condition, callback, timeout = 100) {
 };
 
 
+// init Sentry.io service
 wait(
   () => !!window.Sentry,
   () => Sentry.init({ dsn: SentryDSN }),
 );
+
+
+// add js variables from GET params
+new URL(location).searchParams.forEach((value, key) => {
+  if (window[key + 'js'] === undefined) {
+    window[key + 'js'] = value;
+  }
+});
+
+
+// populate links with GET params
+function populateLinksWithGetParams() {
+  document.querySelectorAll('a[href]').forEach(link => {
+    const href = link.getAttribute('href');
+
+    if (!href.match(/^https?:\/\//) && !link.href.match(/^mailto:/)) {
+      const url = new URL(link.href);
+
+      new URL(location).searchParams.forEach((value, key) => {
+        if (!url.searchParams.has(key)) {
+          url.searchParams.set(key, value);
+        }
+      });
+
+      link.setAttribute('href', url.pathname + url.search + url.hash);
+    }
+  });
+}
+
+if (document.readyState !== 'interactive' && document.readyState !== 'complete') {
+  document.addEventListener('DOMContentLoaded', populateLinksWithGetParams);
+} else {
+  populateLinksWithGetParams();
+}
+
+
+})();
