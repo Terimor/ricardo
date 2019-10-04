a67e5ae6ffaf872c53a4bea712aac3268e7a8fa7
Viacheslav Alenchikov <valenchikov@gmail.com>
2019-10-04 08:59:58 -0400

    card curved images

--- resources/js/mixins/creditCards.js
+++ resources/js/mixins/creditCards.js
@@ -4,16 +4,23 @@ export default {
 
     creditCards() {
       return {
-        visa: { title: 'Visa', image: '/images/cc-icons/visa.png' },
-        mastercard: { title: 'Mastercard', image: '/images/cc-icons/mastercard.png' },
-        americanexpress: { title: 'American Express', image: '/images/cc-icons/american-express.png' },
-        discover: { title: 'Discover', image: '/images/cc-icons/discover.png' },
-        dinersclub: { title: 'Diners Club', image: '/images/cc-icons/diners-club.png' },
-        jcb: { title: 'JCB', image: '/images/cc-icons/jcb.png' },
-        elo: { title: 'Elo', image: '/images/cc-icons/elo.png' },
-        hipercard: { title: 'Hipercard', image: '/images/cc-icons/hipercard.png' },
-        aura: { title: 'Aura', image: '/images/cc-icons/aura.png' },
+        americanexpress: { title: 'American Express', image: '/images/cc-icons/american-express-curved-128px.png' },
+        aura: { title: 'Aura', image: '/images/cc-icons/aura-curved.png' },
+        banccontact: { title: 'Banccontact', image: '/images/cc-icons/banccontact.png' },
+        dinersclub: { title: 'Diners Club', image: '/images/cc-icons/diners-curved.png' },
+        discover: { title: 'Discover', image: '/images/cc-icons/discover-curved-128px.png' },
+        elo: { title: 'Elo', image: '/images/cc-icons/elo-curved.png' },
+        eps: { title: 'EPS', image: '/images/cc-icons/eps-curved.png' },
+        giropay: { title: 'Giropay', image: '/images/cc-icons/giropay-curved.png' },
+        hipercard: { title: 'Hipercard', image: '/images/cc-icons/hipercard-curved.png' },
+        ideal: { title: 'iDEAL', image: '/images/cc-icons/ideal-curved.png' },
+        jcb: { title: 'JCB', image: '/images/cc-icons/jcb-curved.png' },
         maestro: { title: 'Maestro', image: '/images/cc-icons/maestro.png' },
+        mastercard: { title: 'Mastercard', image: '/images/cc-icons/mastercard-curved-128px.png' },
+        multibango: { title: 'Multibango', image: '/images/cc-icons/multibango.png' },
+        prezelewy24: { title: 'Prezelewy24', image: '/images/cc-icons/prezelewy24-curved.png' },
+        sofort: { title: 'Sofort', image: '/images/cc-icons/sofort-curved.png' },
+        visa: { title: 'Visa', image: '/images/cc-icons/visa-curved-128px.png' },
       };
     },
 
