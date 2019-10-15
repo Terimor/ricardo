63f4d345071c952775258377a2c326f34bf6179a
stanislav-karaliov <lorderetik@gmail.com>
2019-10-15 18:43:58 +0300

    test host

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -272,5 +272,11 @@ class SiteController extends Controller
 
         return view('prober', compact('result', 'redis'));
     }
+    
+    public function test(Request $request)
+    {
+        echo '<pre>'; var_dump(request()->getHost()); echo '</pre>';
+        echo '<pre>'; var_dump(request()->server()); echo '</pre>';
+    }
 
 }
