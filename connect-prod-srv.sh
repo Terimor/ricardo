7ef92a84bcfe899154ee97201d839dbf21d5c8ba
Vitaly <vitaly@on-5.com>
2019-10-16 10:26:12 +0300

    Added prod connections

---
+++ connect-prod-srv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i ~/.ssh/ODIN_Prod.pem ubuntu@ec2-52-3-54-183.compute-1.amazonaws.com
---
+++ connect-prod2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i ~/.ssh/ODIN_Prod.pem ubuntu@ec2-3-216-153-170.compute-1.amazonaws.com
