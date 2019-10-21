62b572428b9081439c5f1b2f66dc63a13fac72d1
Viacheslav Alenchikov <webmasterslava@gmail.com>
2019-10-21 13:13:24 +0600

    3ds redirect small fix

--- resources/views/layouts/3ds_redirect.blade.php
+++ resources/views/layouts/3ds_redirect.blade.php
@@ -1,37 +1,32 @@
-@if ($HasVueApp)
-  @if (Request::is('checkout'))
-    <script type="text/javascript">
-      (() => {
-        const url = new URL(window.location);
+@if (Request::is('checkout') && Request::get('3ds') && !Request::get('3ds_restore'))
+  <script type="text/javascript">
+    (() => {
+      const url = new URL(window.location);
+      const params = window.localStorage.getItem('3ds_params') || '';
 
-        if (url.searchParams.has('3ds') && !url.searchParams.has('3ds_restore')) {
-          const params = window.localStorage.getItem('3ds_params') || '';
-
-          new URLSearchParams(params).forEach((value, key) => {
-            if (!url.searchParams.has(key)) {
-              url.searchParams.set(key, value);
-            }
-          });
-
-          if (url.searchParams.get('3ds') === 'failure') {
-            url.searchParams.set('3ds_restore', 1);
-            window.location.href = url.href;
-          }
-
-          if (url.searchParams.get('3ds') === 'success') {
-            url.pathname = '/thankyou-promos';
-            url.searchParams.set('order', localStorage.getItem('odin_order_id'));
-            url.searchParams.set('cur', localStorage.getItem('order_currency'));
-            localStorage.setItem('odin_order_created_at', new Date());
-            url.searchParams.delete('3ds_restore');
-            url.searchParams.delete('3ds');
-
-            window.location.href = url.href;
-          }
-
-          window.stop();
+      new URLSearchParams(params).forEach((value, key) => {
+        if (!url.searchParams.has(key)) {
+          url.searchParams.set(key, value);
         }
-      })();
-    </script>
-  @endif
+      });
+
+      if (url.searchParams.get('3ds') === 'failure') {
+        url.searchParams.set('3ds_restore', 1);
+        window.location.href = url.href;
+      }
+
+      if (url.searchParams.get('3ds') === 'success') {
+        url.pathname = '/thankyou-promos';
+        url.searchParams.set('order', localStorage.getItem('odin_order_id'));
+        url.searchParams.set('cur', localStorage.getItem('order_currency'));
+        localStorage.setItem('odin_order_created_at', new Date());
+        url.searchParams.delete('3ds_restore');
+        url.searchParams.delete('3ds');
+
+        window.location.href = url.href;
+      }
+
+      window.stop();
+    })();
+  </script>
 @endif
