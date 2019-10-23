692ab8b1a23f49ef46e14ed003c6d544aaf62750
Maxim Batanov <mrbatanov@gmail.com>
2019-10-23 18:06:32 +0300

    debug: checkout.com upsells amount

--- app/Services/CheckoutDotComService.php
+++ app/Services/CheckoutDotComService.php
@@ -138,6 +138,9 @@ class CheckoutDotComService
     {
         $amount = isset($amount) ? $amount : $order->total_price;
 
+        logger()->info('Amount-> ' . $amount);
+        logger()->info('Amount normalized-> ' . CheckoutDotComAmountMapper::toProvider($amount, $order->currency));
+
         $payment = new Payment($source, $order->currency);
         $payment->reference = $order->number;
         $payment->amount = CheckoutDotComAmountMapper::toProvider($amount, $order->currency);
