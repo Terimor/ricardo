486b081355762113dc0b0fb7012f172b5e42eb13
Vitaly <vitaly@on-5.com>
2019-11-05 14:59:27 +0300

    Cards icons from new S3 via CDN

--- app/Constants/PaymentMethods.php
+++ app/Constants/PaymentMethods.php
@@ -29,92 +29,92 @@ class PaymentMethods
     public static $list = [
         self::INSTANT_TRANSFER => [
             'name'      => 'PayPal',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/paypal-curved-128px.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/payPal.png',
             'is_active' => true
         ],
         self::MASTERCARD => [
             'name'      => 'MasterCard',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/mastercard-curved-128px.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/mastercard-curved-128px.png',
             'is_active' => true,
         ],
         self::CREDITCARD => [
             'name'      => 'Credit card',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/othercard.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/othercard.png',
             'is_active' => false,
         ],
         self::VISA => [
             'name'      => 'VISA',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/visa-curved-128px.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/visa-curved-128px.png',
             'is_active' => true,
         ],
         self::AMEX => [
             'name'      => 'AmEx',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/american-express-curved-128px.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/american-express-curved-128px.png',
             'is_active' => true,
         ],
         self::IDEAL => [
             'name'      => 'iDeal',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/ideal-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/ideal-curved.png',
             'is_active' => true,
         ],
         self::EPS => [
             'name'      => 'EPS',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/eps-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/eps-curved.png',
             'is_active' => true,
         ],
         self::PREZELEWY24 => [
             'name'      => 'Prezelewy 24',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/prezelewy24-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/prezelewy24-curved.png',
             'is_active' => true,
         ],
         self::JCB => [
             'name'      => 'JCB',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/jcb-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/jcb-curved.png',
             'is_active' => true,
         ],
         self::AURA => [
             'name'      => 'Aura',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/aura-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/aura-curved.png',
             'is_active' => true,
         ],
         self::ELO => [
             'name'      => 'Elo',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/elo-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/eps-curved.png',
             'is_active' => true,
         ],
         self::HIPERCARD => [
             'name'      => 'Hipercard',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/hipercard-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/hipercard-curved.png',
             'is_active' => true,
         ],
         self::DISCOVER  => [
             'name'      => 'Discover',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/discover-curved-128px.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/discover-curved-128px.png',
             'is_active' => true,
         ],
         self::DINERSCLUB => [
             'name'      => 'Diners Club',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/diners-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/diners-curved.png',
             'is_active' => true,
         ],
         self::NARANJA => [
             'name'      => 'Naranja',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/naranja-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/naranja-curved-128px.png',
             'is_active' => true,
         ],
         self::CARNET => [
             'name'      => 'Carnet',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/carnet-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/carnet-curved-128px.png',
             'is_active' => true,
         ],
         self::CABAL => [
             'name'      => 'Cabal',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/cabal-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/cabal-curved-128px.png',
             'is_active' => true,
         ],
         self::CREDIMAS => [
             'name'      => 'Credimas',
-            'logo'      => 'https://static-backend.saratrkr.com/image_assets/credimas-curved.png',
+            'logo'      => 'https://cdn.backenddomainsecure.com/static/images/credimas-curved-128px.png',
             'is_active' => true,
         ]
     ];
