9f1d229520e5827c44ec7a2c11753abe58a8a1d6
Viacheslav Alenchikov <valenchikov@gmail.com>
2019-11-14 22:16:51 +0600

    static topbar translate

--- resources/views/components/static_topbar.blade.php
+++ resources/views/components/static_topbar.blade.php
@@ -1,7 +1,7 @@
 @if (!$HasVueApp)
   <div id="static-topbar" class="hidden">
     <div class="inside">
-      Need help or have a question with a purchase? <a class="openchat">Chat</a> with us, <a href="contact-us">email us or call us anytime</a>!
+      {!! t('static_topbar.content') !!}
     </div>
   </div>
 @endif
