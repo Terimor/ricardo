6df43e734da4f36dbbad3994251ac5cbf307940d
Viacheslav Alenchikov <valenchikov@gmail.com>
2019-11-19 16:46:26 +0600

    document number brazil mask

--- resources/js/components/common/extra-fields/DocumentNumber.vue
+++ resources/js/components/common/extra-fields/DocumentNumber.vue
@@ -11,13 +11,17 @@
       'format': placeholder,
     }"
     class="input-container variant-1"
-    theme="variant-1" />
+    theme="variant-1"
+    @input="input" />
 
 </template>
 
 
 <script>
-  
+
+  let traverseCount = 0;
+
+
   export default {
 
     name: 'DocumentNumber',
@@ -43,6 +47,41 @@
         return typeof this.extraFields.document_number.placeholder === 'object'
           ? this.extraFields.document_number.placeholder[this.form.document_type] || ''
           : this.extraFields.document_number.placeholder;
+      },
+
+    },
+
+
+    methods: {
+
+      input() {
+        if (this.placeholder === 'xxx.xxx.xxx-xx') {
+          traverseCount = 0;
+          this.traverse();
+        }
+      },
+
+      traverse() {
+        let value = this.form.document_number || '';
+        
+        for (let i = 0; i < value.length; i++) {
+          let symbol = this.placeholder.substr(i, 1) || '';
+
+          if (!symbol) {
+            this.form.document_number = value.substr(0, i);
+            return;
+          }
+
+          if (!/[A-z0-9]/.test(symbol) && value[i] !== symbol) {
+            this.form.document_number = value.substr(0, i) + symbol + value.substr(i);
+
+            if (traverseCount++ < 100) {
+              this.traverse();
+            }
+
+            return;
+          }
+        }
       },
 
     },
