052257eb7a8fd065348e403720950aaa5b6361ed
stanislav-karaliov <lorderetik@gmail.com>
2019-12-04 12:26:34 +0300

    sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,52 +2,52 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ route('home') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>daily</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('contact-us') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('returns') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('privacy') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('delivery') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('terms') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('about') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('splash') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>daily</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('checkout') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>daily</changefreq>
         <priority>1</priority>
     </url>
     <url>
         <loc>{{ route('order-tracking') }}</loc>
-        <changefreq>always</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1</priority>
     </url>
 </urlset>
