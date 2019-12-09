0f3bd2ba4ce76d16e8e6017a333fa005a10d4e14
stanislav-karaliov <lorderetik@gmail.com>
2019-12-09 17:40:38 +0300

    prober

--- resources/views/prober.blade.php
+++ resources/views/prober.blade.php
@@ -1,7 +1,7 @@
 <pre>
 <ol>    
         <li>Synced Redis · · · · · · · · · · · · {{ $redis }}</li>
-        <li>Successful transactions . . . . . . . {{ $txns }}%</li>
+        <li>Successful transactions . . . . . . . {{ $txns }}</li>
 </ol>
 <b>{{ $result }}</b>
 </pre>
