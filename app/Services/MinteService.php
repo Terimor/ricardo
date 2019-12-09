851cb09fac770e07b86307eebfcac0f88f0253d8
Maxim Batanov <mrbatanov@gmail.com>
2019-12-09 15:42:26 +0300

    feature: move get api keys to trait

--- app/Services/MinteService.php
+++ app/Services/MinteService.php
@@ -19,6 +19,8 @@ use GuzzleHttp\Exception\RequestException as GuzzReqException;
  */
 class MinteService
 {
+    use ProviderServiceTrait;
+
     const ENV_LIVE      = 'live';
     const ENV_SANDBOX   = 'sandbox';
 
@@ -196,31 +198,6 @@ class MinteService
         }
 
         return $payment;
-    }
-
-    /**
-     * Setup PaymentApi
-     * @param  array $attrs ['product_id'=>string,'payment_api_id'=>string]
-     * @return PaymentApi|null
-     */
-    public function getPaymentApi(array $attrs): ?PaymentApi
-    {
-        $default = collect($this->keys)->first(function($v, $k) {
-            return empty($v->product_ids);
-        });
-
-        $key = null;
-        if (!empty($attrs['product_id'])) {
-            $key = collect($this->keys)->first(function($v, $k) use ($attrs) {
-                return in_array($attrs['product_id'], $v->product_ids);
-            });
-        } elseif (!empty($attrs['payment_api_id'])) {
-            $key = collect($this->keys)->first(function($v, $k) use ($attrs) {
-                return $attrs['payment_api_id'] === (string)$v->getIdAttribute();
-            });
-        }
-
-        return $key ?? $default;
     }
 
     /**
