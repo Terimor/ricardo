7ed494d932d6d90b5f556a22d54249a4cfedd74c
Viacheslav Alenchikov <valenchikov@gmail.com>
2019-12-12 19:37:27 +0600

    returns remove dropdown

--- resources/views/returns.blade.php
+++ resources/views/returns.blade.php
@@ -11,16 +11,6 @@
 <div class="static">
     <div class="container">
         <div class="static__wrapper">
-            <div class="returns-address">
-              <div class="label">{{ t('returns.address.label') }}</div>
-              <select class="selector">
-                <option value="" data-value=""></option>
-                <option value="value1" data-value="{{ t('returns.address.value1') }}">{{ t('returns.address.option1') }}</option>
-                <option value="value2" data-value="{{ t('returns.address.value2') }}">{{ t('returns.address.option2') }}</option>
-                <option value="value3" data-value="{{ t('returns.address.value3') }}">{{ t('returns.address.option3') }}</option>
-              </select>
-              <div class="address"></div>
-            </div>
             {!! t('returns.content') !!}
             @include('layouts.footer', ['isWhite' => true, 'hasHome' => true ])
         </div>
