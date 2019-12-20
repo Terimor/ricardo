586e3d11777d9bc601b371d7efee405fb369bf83
stanislav-karaliov <lorderetik@gmail.com>
2019-12-20 14:13:34 +0300

    prober

--- resources/views/prober.blade.php
+++ resources/views/prober.blade.php
@@ -2,7 +2,7 @@
 <ol>    
         <li>Synced Redis · · · · · · · · · · · · · · · · · {{ $redis }}</li>
         <li>Successful transactions (last {{ $setting['prober_txns_orders'] }}) · · · · · · · {{ $txns }}</li>
-        <li>Firing (last {{ $setting['prober_firing_orders'] }}) · · · · · · · · · · · · · · · {{ $firing }}</li>
+        <li>Firing (last {{ $setting['prober_firing_orders'] }} orders) · · · · · · · · · · · {{ $firing }}</li>
 </ol>
 <b>{{ $result }}</b>
 </pre>
