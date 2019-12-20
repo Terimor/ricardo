86c3590699b40971fb2f91ca77ca616e072bbcae
stanislav-karaliov <lorderetik@gmail.com>
2019-12-20 21:24:28 +0300

    80 refuse limit

--- app/Constants/PaymentProviders.php
+++ app/Constants/PaymentProviders.php
@@ -27,7 +27,7 @@ class PaymentProviders
             'fraud_setting' => [
                 '3ds_limit' => 85,
                 'fallback_limit' => 0,
-                'refuse_limit' => 99
+                'refuse_limit' => 80
             ],
             'methods'   => [
                 'main' => [
@@ -40,14 +40,14 @@ class PaymentProviders
         ],
         self::PAYPAL_HK => [
             'name'      => 'PayPal HK',
-            'is_active' => true,
+            'is_active' => false,
             'is_main'   => true,
             'is_fallback' => false,
             'in_prod'   => true,
             'fraud_setting' => [
                 '3ds_limit' => 85,
                 'fallback_limit' => 0,
-                'refuse_limit' => 99
+                'refuse_limit' => 80
             ],
             'methods'   => [
                 'main' => [
