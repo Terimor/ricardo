6193629300b148cc54caa0585d2b93e8ffa84e8e
stanislav-karaliov <lorderetik@gmail.com>
2020-01-13 10:52:16 +0000

    Squashed commit of the following
    commit de3c7be7 
    Author: stanislav-karaliov <lorderetik@gmail.com> 
    Date: Mon Jan 13 2020 13:50:57 GMT+0300 (Москва, стандартное время) 
    
        Timeout = 2
    
    
    commit e1a418a2 
    Author: stanislav-karaliov <lorderetik@gmail.com> 
    Date: Mon Jan 13 2020 13:49:41 GMT+0300 (Москва, стандартное время) 
    
        Timeout = 2
    
    
    commit 46f52974 
    Author: stanislav-karaliov <lorderetik@gmail.com> 
    Date: Mon Jan 13 2020 13:21:47 GMT+0300 (Москва, стандартное время) 
    
        Timeout = 2
    
    
    commit 10a45676 
    Author: stanislav-karaliov <lorderetik@gmail.com> 
    Date: Mon Jan 13 2020 13:18:22 GMT+0300 (Москва, стандартное время) 
    
        Timeout = 2

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -13,36 +13,37 @@ use App\Exceptions\BlockEmailException;
  */
 class EmailService
 {
-    
+
     public static $thecheckerEmailValidStatus = 'deliverable';
     public static $ipqsLowDeliverability = 'low';
     public static $NA = 'N/A';
-    
+    const TIMEOUT_IPQS = 2;
+
     /**
      *
-     * @var type 
+     * @var type
      */
     protected $apiKey;
 
     /**
-     * 
+     *
      */
     public function __construct()
     {
         $apiKey = Setting::getValue('saga_api_access_key');
         $this->apiKey = !empty($apiKey) ? $apiKey : '';
     }
-    
+
     /**
      * Validate email with thecheker.io
      * @param string $email
      * @return bool
      */
     public function validateEmailWithThechecker(string $email) : bool
-    {        
+    {
         $apiKey = Setting::getValue('thechecker_api_key');
         // validate email using php
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {                
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $url = "https://api.thechecker.co/v2/verify?email={$email}&api_key={$apiKey}";
             $result = file_get_contents($url);
             $res = json_decode($result);
@@ -58,11 +59,11 @@ class EmailService
             }
         } else {
             $isValid = false;
-        }        
-        
+        }
+
         return $isValid;
     }
-    
+
     /**
      * Validate Email with ipqualityscore.com
      * @param string $email
@@ -73,20 +74,20 @@ class EmailService
         $apiKey = Setting::getValue('ipqs_private_api_key');
         $block = false; $suggest = ''; $warning = false; $valid = false; $disposable = false;
         if ($email) {
-            $url =  "https://www.ipqualityscore.com/api/json/email/{$apiKey}/{$email}";
+            $url =  "https://www.ipqualityscore.com/api/json/email/{$apiKey}/{$email}?timeout=".static::TIMEOUT_IPQS;
             $timeOut = stream_context_create(
-                ['http'=> ['timeout' => 3 ]]
+                ['http'=> ['timeout' => 3]]
             );
             for ($i=1; $i<=3; $i++) {
                 try {
-                    $result = file_get_contents($url, false, $timeOut);                
-                } catch (\Exception $ex) {                    
+                    $result = file_get_contents($url, false, $timeOut);
+                } catch (\Exception $ex) {
                     //logger()->error("Validate email IPQS connection error", ['email' => $email,'code' => $ex->getCode(), 'message' => $ex->getMessage()]);
                 }
                 if (isset($result)) {
-                    $res = json_decode($result);                    
+                    $res = json_decode($result);
                     if ($res) {
-                        // check warning 
+                        // check warning
                         if (!empty($res->timed_out) || $res->deliverability == static::$ipqsLowDeliverability) {
                             $warning = true;
                         }
@@ -105,7 +106,7 @@ class EmailService
                         // block if recent_abuse, leaked or overall_score = 0
                         if (!empty($res->recent_abuse) || !empty($res->leaked)) {
                             $block = true;
-                            logger()->info("Blocked email", ['email' => $email, 'res' => $res]);                            
+                            logger()->info("Blocked email", ['email' => $email, 'res' => $res]);
                             throw new BlockEmailException([
                                 'block' => $block,
                                 'warning' => $warning,
@@ -113,7 +114,7 @@ class EmailService
                                 'valid' => $valid,
                                 'disposable' => $disposable
                             ], "Email blocked {$email}, answer: ".json_encode($res));
-                        }                        
+                        }
 
                         break;
                     }
@@ -131,6 +132,6 @@ class EmailService
             'disposable' => $disposable
         ];
     }
-    
+
 
 }
