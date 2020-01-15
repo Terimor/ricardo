be40e10411f0b37272a3088ed0c0f580cc9e2ea2
Viacheslav Alenchikov <valenchikov@gmail.com>
2020-01-15 16:10:15 +0600

    ie11 sentry 4

--- resources/views/components/scripts/sentry.blade.php
+++ resources/views/components/scripts/sentry.blade.php
@@ -1,38 +1,32 @@
-@if (isset($sentry_dsn) && config('app.env') !== 'local' && config('app.env') !== 'development' && str_replace('www.', '', Request::getHost()) !== 'smartbell.pro')
+@if (true || isset($sentry_dsn) && config('app.env') !== 'local' && config('app.env') !== 'development' && str_replace('www.', '', Request::getHost()) !== 'smartbell.pro')
 
   <script
-    src="https://browser.sentry-cdn.com/5.6.3/bundle.min.js"
+    src="https://browser.sentry-cdn.com/5.11.0/bundle.min.js"
     onload="js_deps.ready('sentry')"
     crossorigin="anonymous"
     async></script>
 
+  @if (!empty($HasVueApp) || !empty($is_minishop))
+    <script
+      src="https://browser.sentry-cdn.com/5.11.0/vue.min.js"
+      onload="js_deps.ready('sentry-vue')"
+      crossorigin="anonymous"
+      async></script>
+  @endif
+
   <script type="text/javascript">
     @if (!empty($HasVueApp) || !empty($is_minishop))
-      js_deps.wait(['vue', 'sentry', 'page-scripts'], function() {
-        var script = document.createElement('script');
-        script.src = 'https://browser.sentry-cdn.com/5.6.3/vue.min.js';
-        script.onload = js_deps.ready('sentry-vue');
-        script.crossorigin = 'anonymous';
-        script.async = true;
-        document.head.appendChild(script);
-
-        js_deps.wait_for(
-          function() {
-            return !!Sentry.Integrations.Vue;
-          },
-          function() {
-            Sentry.init({
-              dsn: '{{ $sentry_dsn }}',
-              integrations: [
-                new Sentry.Integrations.Vue({
-                  Vue: Vue,
-                  attachProps: true,
-                  logErrors: true,
-                }),
-              ],
-            });
-          }
-        );
+      js_deps.wait(['vue', 'sentry', 'sentry-vue'], function() {
+        Sentry.init({
+          dsn: '{{ $sentry_dsn }}',
+          integrations: [
+            new Sentry.Integrations.Vue({
+              Vue: Vue,
+              attachProps: true,
+              logErrors: true,
+            }),
+          ],
+        });
       });
     @else
       js_deps.wait(['sentry'], function() {
