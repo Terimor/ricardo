a2b7aea2c31779bd0830fe9a7c8767eebf191c83
Maxim Batanov <mrbatanov@gmail.com>
2020-02-06 16:16:52 +0300

    fix: rename PI to PH

--- app/Constants/PaymentProviders.php
+++ app/Constants/PaymentProviders.php
@@ -345,8 +345,8 @@ class PaymentProviders
                         'excl' => [
                             'ae', 'af', 'ag', 'al', 'ar', 'au', 'bg', 'br', 'bt', 'bz', 'ca', 'cf', 'cc', 'ch', 'ck', 'cl', 'co', 'cx', 'cz',
                             'dk', 'do', 'gb', 'gl', 'gp', 'gr', 'hk', 'hm', 'hr', 'hu', 'il', 'in', 'je', 'jo', 'jp', 'ki', 'kr', 'kz', 'li',
-                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'pi', 'pl', 'pn', 'ro', 'rp', 'ru', 'se', 'sg', 'sy', 'tk',
-                            'tt', 'tr', 'tv', 'tw', 'za', 'us', 'uz'
+                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'ph', 'pl', 'pn', 'ro', 'ru', 'se', 'sg', 'sy', 'tk', 'tt',
+                            'tr', 'tv', 'tw', 'za', 'us', 'uz'
                             // 'af', 'ag', 'al', 'ar', 'br', 'bz', 'ca', 'cf', 'co', 'do', 'gb', 'gl', 'je', 'jo', 'kz', 'mx', 'sy', 'tt', 'us', 'uz'
                         ]
                     ],
@@ -359,8 +359,8 @@ class PaymentProviders
                         'excl' => [
                             'ae', 'af', 'ag', 'al', 'ar', 'au', 'bg', 'br', 'bt', 'bz', 'ca', 'cf', 'cc', 'ch', 'ck', 'cl', 'co', 'cx', 'cz',
                             'dk', 'do', 'gb', 'gl', 'gp', 'gr', 'hk', 'hm', 'hr', 'hu', 'il', 'in', 'je', 'jo', 'jp', 'ki', 'kr', 'kz', 'li',
-                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'pi', 'pl', 'pn', 'ro', 'rp', 'ru', 'se', 'sg', 'sy', 'tk',
-                            'tt', 'tr', 'tv', 'tw', 'za', 'us', 'uz'
+                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'ph', 'pl', 'pn', 'ro', 'ru', 'se', 'sg', 'sy', 'tk', 'tt',
+                            'tr', 'tv', 'tw', 'za', 'us', 'uz'
                             // 'af', 'ag', 'al', 'ar', 'br', 'bz', 'ca', 'cf', 'co', 'do', 'gb', 'gl', 'je', 'jo', 'kz', 'mx', 'sy', 'tt', 'us', 'uz'
                         ]
                     ],
@@ -1042,8 +1042,8 @@ class PaymentProviders
                         'excl' => [
                             'ae', 'af', 'ag', 'al', 'ar', 'au', 'bg', 'br', 'bt', 'bz', 'ca', 'cf', 'cc', 'ch', 'ck', 'cl', 'co', 'cx', 'cz',
                             'dk', 'do', 'gb', 'gl', 'gp', 'gr', 'hk', 'hm', 'hr', 'hu', 'il', 'in', 'je', 'jo', 'jp', 'ki', 'kr', 'kz', 'li',
-                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'pi', 'pl', 'pn', 'ro', 'rp', 'ru', 'se', 'sg', 'sy', 'tk',
-                            'tt', 'tr', 'tv', 'tw', 'za', 'us', 'uz'
+                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'ph', 'pl', 'pn', 'ro', 'ru', 'se', 'sg', 'sy', 'tk', 'tt',
+                            'tr', 'tv', 'tw', 'za', 'us', 'uz'
                         ]
                     ],
                     PaymentMethods::MASTERCARD => [
@@ -1055,8 +1055,8 @@ class PaymentProviders
                         'excl' => [
                             'ae', 'af', 'ag', 'al', 'ar', 'au', 'bg', 'br', 'bt', 'bz', 'ca', 'cf', 'cc', 'ch', 'ck', 'cl', 'co', 'cx', 'cz',
                             'dk', 'do', 'gb', 'gl', 'gp', 'gr', 'hk', 'hm', 'hr', 'hu', 'il', 'in', 'je', 'jo', 'jp', 'ki', 'kr', 'kz', 'li',
-                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'pi', 'pl', 'pn', 'ro', 'rp', 'ru', 'se', 'sg', 'sy', 'tk',
-                            'tt', 'tr', 'tv', 'tw', 'za', 'us', 'uz'
+                            'ls', 'mx', 'my', 'na', 'nf', 'no', 'nr', 'nu', 'nz', 'ph', 'pl', 'pn', 'ro', 'ru', 'se', 'sg', 'sy', 'tk', 'tt',
+                            'tr', 'tv', 'tw', 'za', 'us', 'uz'
                         ]
                     ],
                     PaymentMethods::AMEX => [
@@ -1418,8 +1418,8 @@ class PaymentProviders
                         '-3ds' => [
                             'ae', 'ar', 'au', 'gb', 'at', 'be', 'bg', 'bt', 'ca', 'cc', 'ch', 'ck', 'cl', 'co', 'cx', 'cy', 'cz', 'de', 'dk',
                             'ee', 'es', 'fi', 'fr', 'gb', 'gp', 'gr', 'hk', 'hm', 'hr', 'hu', 'ie', 'il', 'in', 'it', 'jp', 'ki', 'kr', 'li',
-                            'ls', 'lt', 'lu', 'lv', 'mc', 'mt', 'mx', 'my', 'na', 'nf', 'nl', 'no', 'nr', 'nu', 'nz', 'pi', 'pl', 'pn', 'pt',
-                            'ro', 'rp', 'ru', 'se', 'sg', 'si', 'sk', 'sm', 'tk', 'tr', 'tv', 'tw', 'us', 'va', 'za'
+                            'ls', 'lt', 'lu', 'lv', 'mc', 'mt', 'mx', 'my', 'na', 'nf', 'nl', 'no', 'nr', 'nu', 'nz', 'ph', 'pl', 'pn', 'pt',
+                            'ro', 'ru', 'se', 'sg', 'si', 'sk', 'sm', 'tk', 'tr', 'tv', 'tw', 'us', 'va', 'za'
                         ],
                         'excl' => ['af', 'ag', 'al', 'ar', 'br', 'bz', 'cf', 'co', 'do', 'gl', 'je', 'jo', 'kz', 'mx', 'ru', 'sy', 'tt', 'uz']
                     ],
@@ -1427,8 +1427,8 @@ class PaymentProviders
                         '-3ds' => [
                             'ae', 'ar', 'au', 'gb', 'at', 'be', 'bg', 'bt', 'ca', 'cc', 'ch', 'ck', 'cl', 'co', 'cx', 'cy', 'cz', 'de', 'dk',
                             'ee', 'es', 'fi', 'fr', 'gb', 'gp', 'gr', 'hk', 'hm', 'hr', 'hu', 'ie', 'il', 'in', 'it', 'jp', 'ki', 'kr', 'li',
-                            'ls', 'lt', 'lu', 'lv', 'mc', 'mt', 'mx', 'my', 'na', 'nf', 'nl', 'no', 'nr', 'nu', 'nz', 'pi', 'pl', 'pn', 'pt',
-                            'ro', 'rp', 'ru', 'se', 'sg', 'si', 'sk', 'sm', 'tk', 'tr', 'tv', 'tw', 'us', 'va', 'za'
+                            'ls', 'lt', 'lu', 'lv', 'mc', 'mt', 'mx', 'my', 'na', 'nf', 'nl', 'no', 'nr', 'nu', 'nz', 'ph', 'pl', 'pn', 'pt',
+                            'ro', 'ru', 'se', 'sg', 'si', 'sk', 'sm', 'tk', 'tr', 'tv', 'tw', 'us', 'va', 'za'
                         ],
                         'excl' => ['af', 'ag', 'al', 'ar', 'br', 'bz', 'cf', 'co', 'do', 'gl', 'je', 'jo', 'kz', 'mx', 'sy', 'tt', 'uz']
                     ]
