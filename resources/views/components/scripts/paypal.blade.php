e9390b79a1281351479e400d91029b67c0ca8bfd
Viacheslav Alenchikov <valenchikov@gmail.com>
2020-02-28 16:49:57 +0600

    paypal delayed loading

--- resources/views/components/scripts/paypal.blade.php
+++ resources/views/components/scripts/paypal.blade.php
@@ -1,9 +1,9 @@
 @if (!$product->is_paypal_hidden && (!empty($is_checkout) || !empty($is_upsells)))
 
-  <script type="text/javascript" id="paypal-script">
-    setTimeout(function() {
-      js_deps.add_script('paypal', 'https://www.paypal.com/sdk/js?currency={{$PayPalCurrency}}&disable-card=visa,mastercard,amex&client-id={{ $setting['instant_payment_paypal_client_id'] }}');
-    }, 1000);
-  </script>
+  <script
+    id="paypal-script"
+    src="https://www.paypal.com/sdk/js?currency={{$PayPalCurrency}}&disable-card=visa,mastercard,amex&client-id={{ $setting['instant_payment_paypal_client_id'] }}"
+    onload="js_deps.ready('paypal')"
+    async></script>
 
 @endif
