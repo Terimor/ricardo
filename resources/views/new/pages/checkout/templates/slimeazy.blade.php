24f959c6d9274650c33eb615439f62f79cdbbd5e
Viacheslav Alenchikov <valenchikov@gmail.com>
2020-03-01 22:40:34 +0600

    slimeazy section3

--- resources/views/new/pages/checkout/templates/slimeazy.blade.php
+++ resources/views/new/pages/checkout/templates/slimeazy.blade.php
@@ -47,7 +47,18 @@
     </div>
     <div class="section3 section-back">
       <div class="container">
-        
+        <div class="left">
+          <div class="title">A Leader In The Female</div>
+          <div class="subtitle">Diets Industry</div>
+          <div class="image1"><img class="lazy" data-src="{{ $cdn_url }}/assets/images/checkout/slimeazy/natural-img.png" /></div>
+          <div class="image2"><img class="lazy" data-src="{{ $cdn_url }}/assets/images/checkout/slimeazy/side-effct-img.png" /></div>
+          <div class="subtitle2">HCA makes perfect sense - it naturally improves mood, suppresses appetite, and burns fat!</div>
+          <div class="title2">Important Information</div>
+          <div class="text">Pure Garcinia Cambogia Premium is a triple-threat natural health supplement. A combination of suppressed appetite, reduced fat production, and increased serotonin.<br><br>By taking Pure Garcinia Cambogia Premium you prevent the enzyme citrate lysase from taking effect in your body, which is a critical catalyst for converting excess carbs into fat.<br><br>The natural HCA in Pure Garcinia Cambogia Premium is known to reduce appetite and proven to reduce belly fat, but it also improves overall health as well.</div>
+        </div>
+        <div class="right">
+          <img class="image lazy" data-src="{{ $cdn_url }}/assets/images/checkout/slimeazy/import-rghtimg.png" />
+        </div>
       </div>
     </div>
     <div class="section4 section-cover" style="background: url({{ $cdn_url }}/assets/images/checkout/slimeazy/about-bg.jpg) top center no-repeat">
