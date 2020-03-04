9aa1ed9b1cbfaf01f37b80ff8d4c9a0ecd985a45
Arsen Goginian <arsengoginian@gmail.com>
2020-03-04 15:41:11 +0400

    fix new-customer csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -35,6 +35,7 @@ class VerifyCsrfToken extends Middleware
         'log-data',
         'apply-discount',
         /* test routes */
-        'test-payments'
+        'test-payments',
+        'new-customer'
     ];
 }
