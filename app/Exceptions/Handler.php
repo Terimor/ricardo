13a709ab6bec326ed588f4dfb13653c265a00615
Arsen Goginian <arsengoginian@gmail.com>
2020-03-05 18:29:16 +0400

    turn off logger

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,9 +49,9 @@ class Handler extends ExceptionHandler
             //log to sentry in production only
             app('sentry')->captureException($exception);
             // create a log Telegram
-            $log = new Logger('Odin');
-            $log->pushHandler(new TelegramBotHandler('896776756:AAFUu5a9lbMizty2IXKyfG7bMy988Vm0NmU', '-1001271143925'));
-            $log->error($exception);
+//            $log = new Logger('Odin');
+//            $log->pushHandler(new TelegramBotHandler('896776756:AAFUu5a9lbMizty2IXKyfG7bMy988Vm0NmU', '-1001271143925'));
+//            $log->error($exception);
         } else {
             parent::report($exception);
         }
