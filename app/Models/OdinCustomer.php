a9b1c4c2ff548abddfb86ad3316fe716c78d83ec
Arsen Goginian <arsengoginian@gmail.com>
2020-03-05 13:21:13 +0400

    change fingerprint

--- app/Models/OdinCustomer.php
+++ app/Models/OdinCustomer.php
@@ -34,14 +34,14 @@ class OdinCustomer extends Model
 		'doc_ids' => [], // array of strings //documents numbers array
         'language' => null, // enum string
         'addresses' => [
-            'country' => null, // enum string
-            'zip' => null, // string
-            'state' => null, // string
-            'city' => null, // string
-            'street' => null, // string
-            'street2' => null, // string
-            'building' => null, // string
-	        'apt' => null, // string
+//            'country' => null, // enum string
+//            'zip' => null, // string
+//            'state' => null, // string
+//            'city' => null, // string
+//            'street' => null, // string
+//            'street2' => null, // string
+//            'building' => null, // string
+//	        'apt' => null, // string
         ],
         'paypal_payer_id' => null, // string
 		'number' => null, // *U (UXXXXXXXUS, X = A-Z0-9, US = country),
