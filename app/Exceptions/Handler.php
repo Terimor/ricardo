e8baeae0b1418922cb074b9b154685b0feb79237
Arsen Goginian <arsengoginian@gmail.com>
2020-03-06 13:05:54 +0400

    Disabled telegram loger

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,13 +49,13 @@ class Handler extends ExceptionHandler
             //log to sentry in production only
             app('sentry')->captureException($exception);
             // create a log Telegram
-            try {
-                $log = new Logger('Odin');
-                $log->pushHandler(new TelegramBotHandler('896776756:AAFUu5a9lbMizty2IXKyfG7bMy988Vm0NmU', '-1001271143925'));
-                $log->error(substr((string) $exception, 0,4000));
-            } catch (\Exception $e) {
-                logger()->warning($e->getMessage());
-            }
+//            try {
+//                $log = new Logger('Odin');
+//                $log->pushHandler(new TelegramBotHandler('896776756:AAFUu5a9lbMizty2IXKyfG7bMy988Vm0NmU', '-1001271143925'));
+//                $log->error(substr((string) $exception, 0,4000));
+//            } catch (\Exception $e) {
+//                logger()->warning($e->getMessage());
+//            }
         } else {
             parent::report($exception);
         }
