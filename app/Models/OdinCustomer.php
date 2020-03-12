3a9954507915f35cd19c736c0b528c7d20862fcc
stanislav-karaliov <lorderetik@gmail.com>
2020-03-12 10:04:23 +0300

    fix fillable fields exclude arrays

--- app/Models/OdinCustomer.php
+++ app/Models/OdinCustomer.php
@@ -78,7 +78,7 @@ class OdinCustomer extends Model
     * @var array
     */
    protected $fillable = [
-       'email', 'number', 'type', 'first_name', 'last_name', 'fingerprints', 'language', 'paypal_payer_id', 'addresses', 'doc_ids', 'phones',
+       'email', 'number', 'type', 'first_name', 'last_name', 'language', 'paypal_payer_id',
        'last_page_checkout', 'last_viewed_sku_code', 'recovery_way'
    ];
 
