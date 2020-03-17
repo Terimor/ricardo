a2e6a0c009cd03f8e62eff5243e1c1a4728467bb
stanislav-karaliov <lorderetik@gmail.com>
2020-03-17 09:07:12 +0300

    Fix duplicate emails usleep

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -16,7 +16,7 @@ class CustomerService
      */
     public function addOrUpdate(array $data, bool $returnModel = false): array
     {
-        usleep(rand(100000, 500000));
+        usleep(rand(1000, 100000));
         $model = OdinCustomer::firstOrCreate(['email' => strtolower($data['email'])]);
         $model->fill($data);
         if (!empty($data['page'])) {
