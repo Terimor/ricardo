d5e60f92c38685639531ed518252f2375dfe6523
stanislav-karaliov <lorderetik@gmail.com>
2020-03-17 14:55:47 +0300

    usleep

--- app/Http/Controllers/OdinCustomerController.php
+++ app/Http/Controllers/OdinCustomerController.php
@@ -16,7 +16,8 @@ class OdinCustomerController extends Controller
      * @return array
      */
     public function addOrUpdate(OdinCustomerRequest $request, CustomerService $customerService) {
-        usleep(rand(100000, 200000));
+        // wait random time to exclude requests at the same time
+        usleep(rand(1000, 200000));
         $customerService->addOrUpdate($request->except(OdinCustomer::$exceptFromRequest));
         return '';
     }
