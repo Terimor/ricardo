1517e07e895788666973e994c9d4d9e450c6073d
Maxim Batanov <mrbatanov@gmail.com>
2020-03-30 12:00:26 +0300

    fix: lost % in prober

--- resources/views/prober.blade.php
+++ resources/views/prober.blade.php
@@ -2,7 +2,7 @@
     <ol>
         <li>{{ str_pad('Synced Redis', 60, ' .', STR_PAD_RIGHT) }} {{ $redis }}</li>
         <li>{{ str_pad("Successful transactions (last {$setting['prober_orders_count']} orders)", 60, ' .', STR_PAD_RIGHT) }} {{ $success_orders }}%</li>
-        <li>{{ str_pad("Firing (last {$setting['prober_firing_orders']} orders)", 60, ' .', STR_PAD_RIGHT) }} {{ $firing }}</li>
+        <li>{{ str_pad("Firing (last {$setting['prober_firing_orders']} orders)", 60, ' .', STR_PAD_RIGHT) }} {{ $firing }}%</li>
         <li>Successful transactions (last {{$setting['prober_orders_count']}} orders)
             <ul>
                 @foreach ($txn_result as $item)
