18d14ce1d4fbd8037f8928b016f247b2133ffef0
Maxim Batanov <mrbatanov@gmail.com>
2020-04-01 21:07:51 +0300

    polish stripe

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -120,11 +120,10 @@ class PaymentService
             'order_amount'   => $payment['value'],
             'order_id'       => $order->getIdAttribute(),
             'status'         => self::STATUS_FAIL,
-            'id'             => null
+            'id'             => $payment['hash'] ?? null
         ];
 
         if ($payment['status'] !== Txn::STATUS_FAILED) {
-            $result['id'] = $payment['hash'];
             $result['status'] = self::STATUS_OK;
             $result['redirect_url'] = !empty($payment['redirect_url']) ? stripslashes($payment['redirect_url']) : null;
             $result['bs_pf_token'] = $payment['bs_pf_token'] ?? null;
