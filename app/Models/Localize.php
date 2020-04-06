e8bcf1fb31669fb50a42859741c1d04feee0c35a
stanislav-karaliov <lorderetik@gmail.com>
2020-04-06 10:26:49 +0300

    add country to reviews

--- app/Models/Localize.php
+++ app/Models/Localize.php
@@ -14,9 +14,11 @@ class Localize extends Model
     {
         if (!empty($this->reviews) && is_array($this->reviews) && $cities) {
             $reviews = [];
+            $countryCode = \Utils::getLocationCountryCode();
             foreach ($this->reviews as $key => $review) {
                 $reviews[$key] = $review;
                 $reviews[$key]['city'] = $cities[$key] ?? '';
+                $reviews[$key]['country'] = \Utils::$countryCodes[$countryCode] ?? '';
             }
             $this->reviews = $reviews;
         }
