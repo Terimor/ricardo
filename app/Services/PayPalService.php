600bbc367704621152bd6702f9f3edf11caeba28
stanislav-karaliov <lorderetik@gmail.com>
2020-04-16 08:54:20 +0300

    add trusted proxy and log details

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -471,7 +471,7 @@ class PayPalService
 
             if (!$order) {
                 logger()->error(
-                    'Cant find matching order for txh.hash: ' . $paypal_order->id,
+                    '***WerifyOrder1.Cant find matching order for txh.hash: ' . $paypal_order->id,
                     ['paypal_order' => $paypal_order]
                 );
                 abort(404);
@@ -511,7 +511,7 @@ class PayPalService
                 return ['order_id' => $order->id];
             } else {
                 logger()->error(
-                    'Cant find matching order for txh.hash: ' . $paypal_order->id,
+                    '***WerifyOrder2.Cant find matching order for txh.hash: ' . $paypal_order->id,
                     ['paypal_order' => $paypal_order]
                 );
                 abort(404);
@@ -578,7 +578,7 @@ class PayPalService
 
                 if (!$order) {
                     logger()->error(
-                        'Cant find matching order for txh.hash: ' . $paypal_order->id,
+                        '***Webhooks.Cant find matching order for txh.hash: ' . $paypal_order->id,
                         ['paypal_order' => $paypal_order]
                     );
                     abort(404);
