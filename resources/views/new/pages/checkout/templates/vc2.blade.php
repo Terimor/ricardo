8a6a4707f47845910a53b5c310029f13d721df84
imgeorgius129 <imgeorgius@gmail.com>
2020-06-08 18:32:49 +0300

    checkout vc2 payment controls to page bottom

--- resources/views/new/pages/checkout/templates/vc2.blade.php
+++ resources/views/new/pages/checkout/templates/vc2.blade.php
@@ -21,10 +21,52 @@
 @section('content_checkout')
   <div class="vc2" v-cloak>
     @include('new.pages.checkout.templates.vc2.htitle')
+    
     <div class="content">
       @include('new.pages.checkout.templates.vc2.left_column')
       @include('new.pages.checkout.templates.vc2.right_column')
     </div>
+    
+    <div class="payment-controls" v-if="!isPurchasAlreadyExists">
+      <div v-if="form.payment_provider && form.payment_provider !== 'paypal'">
+        @include('new.pages.checkout.form.terms')
+      </div>
+
+      <div
+        v-if="form.payment_provider" 
+        v-show="form.payment_provider === 'paypal'"
+        class="paypal-payment-block"
+      >
+
+        @include('new.components.error', [
+          'ref' => 'paypal_payment_error',
+          'active' => 'payment_error && form.payment_provider === \'paypal\'',
+          'class' => 'paypal-payment-error',
+          'label_code' => 'payment_error',
+        ])
+
+        @include('new.pages.checkout.payment.paypal_button')
+      </div>
+
+      <div
+        v-if="form.payment_provider" 
+        v-show="form.payment_provider !== 'paypal'"
+        class="payment-block"
+      >
+
+        @include('new.components.error', [
+          'ref' => 'payment_error',
+          'active' => 'payment_error',
+          'class' => 'payment-error',
+          'label_code' => 'payment_error',
+        ])
+
+        @include('new.pages.checkout.payment.pay_card_button', ['label' => t('vc2.pay_button')])
+      </div>
+
+      @include('new.pages.checkout.blocks.safe_payment')
+    </div>
+
     @include('new.pages.checkout.templates.vc2.guarantee')
     @include('new.pages.checkout.blocks.safe_invoice')
   </div>
--- resources/views/new/pages/checkout/templates/vc2/right_column.blade.php
+++ resources/views/new/pages/checkout/templates/vc2/right_column.blade.php
@@ -9,57 +9,25 @@
     @include('new.pages.checkout.form.installments')
 
     <div class="product">
-      <div class="name">{!! $product->product_name !!}</div>
-      <div class="text">
-        <i class="fa fa-download"></i>
-        {!! t('vc2.product.text') !!}
+      <div class="product-summary">
+        <div class="name">{!! $product->product_name !!}</div>
+        
+        <div class="text">
+          <i class="fa fa-download"></i>
+          {!! t('vc2.product.text') !!}
+        </div>
+        
+        <div class="total"><span v-cloak>@{{ xprice_total_text }}</span></div>
       </div>
-      <div class="total"><span v-cloak>@{{ xprice_total_text }}</span></div>
+
+      <div class="product-image-wrap">
+        <img src="{{ !empty($product->image) && is_array($product->image) ? $product->image[0] : '' }}" alt="" class="product-image">
+      </div>
     </div>
 
     <div class="access">
       {!! t('vc2.product.access') !!}
-    </div>
-    
-    <div
-      v-if="form.payment_provider && form.payment_provider !== 'paypal'" 
-    >
-      @include('new.pages.checkout.form.terms')
-    </div>
-
-    <div
-      v-if="form.payment_provider" 
-      v-show="form.payment_provider === 'paypal'"
-      class="paypal-payment-block"
-    >
-
-      @include('new.components.error', [
-        'ref' => 'paypal_payment_error',
-        'active' => 'payment_error && form.payment_provider === \'paypal\'',
-        'class' => 'paypal-payment-error',
-        'label_code' => 'payment_error',
-      ])
-
-      @include('new.pages.checkout.payment.paypal_button')
-    </div>
-
-    <div
-      v-if="form.payment_provider" 
-      v-show="form.payment_provider !== 'paypal'"
-      class="payment-block"
-    >
-
-      @include('new.components.error', [
-        'ref' => 'payment_error',
-        'active' => 'payment_error',
-        'class' => 'payment-error',
-        'label_code' => 'payment_error',
-      ])
-
-      @include('new.pages.checkout.payment.pay_card_button', ['label' => t('vc2.pay_button')])
-    </div>
-
-    @include('new.pages.checkout.blocks.safe_payment')
+    </div>    
   </template>
 
   <purchas-already-exists v-else />
