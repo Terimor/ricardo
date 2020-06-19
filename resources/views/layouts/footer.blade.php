435896f8e01d1a1fc236b09b4aa48998b865b1ce
imgeorgius129 <imgeorgius@gmail.com>
2020-06-19 13:33:04 +0300

    footer hidden fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,6 +5,7 @@
             [
                 'text' => t('footer.contact'),
                 'link' => '/contact-us',
+                'isHidden' => $product->type == 'virtual'
             ],
             [
                 'text' => t('footer.terms'),
@@ -16,25 +17,35 @@
             ],
         ];
         @endphp
+        
         @if (isset($hasHome) && $hasHome == true)
             <li class="footer__row-item">
                 <a href="/" class="footer__link">{{ t('footer.home') }}</a>
             </li>
         @endif
+
         @foreach ($links as $item)
-            <li class="footer__row-item">
-                <a href="{{ $item['link'] }}" class="footer__link">{{ $item['text'] }}</a>
-            </li>
+            @if (!$item['isHidden'])
+                <li class="footer__row-item">
+                    <a href="{{ $item['link'] }}" class="footer__link">{{ $item['text'] }}</a>
+                </li>
+            @endif
         @endforeach
+        
         <br/>
+        
         @if (!isset($aff['is_signup_hidden']) || !$aff['is_signup_hidden'])
             <li class="footer__row-item">
                 <a href="https://www.h8m8.com" target="_blank" class="footer__link">{{ t('footer.affiliate') }}</a>
             </li>
         @endif
-        <li class="footer__row-item">
-            <a href="/delivery" class="footer__link">{{ t('footer.delivery') }}</a>
-        </li>
+
+        @if ($product->type != 'virtual')
+            <li class="footer__row-item">
+                <a href="/delivery" class="footer__link">{{ t('footer.delivery') }}</a>
+            </li>
+        @endif
+
         <li class="footer__row-item">
             <a href="/report-abuse" class="footer__link">{{ t('abuse.title') }}</a>
         </li>
