36400dd0cddadee665b60da51c1c47ae454f5ac2
Vladyslav <vkyrychenko@aer.de>
2021-04-01 22:46:31 +0300

    init commit

---
+++ resources/js/new/pages/checkout/templates/nmc9.js
@@ -0,0 +1,22 @@
+import checkout from '../../checkout';
+
+
+js_deps.wait(['vue'], () => {
+    new Vue({
+
+        el: '#app',
+
+
+        mixins: [
+            checkout,
+        ],
+
+
+        validations() {
+            return {
+                ...checkout.validations.call(this),
+            };
+        },
+
+    });
+});
